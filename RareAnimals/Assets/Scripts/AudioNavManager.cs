5280777ee330b75839a9fd4cd925f5c249a02dba
Home Pc <377818742@qq.com>
2018-01-14 20:14:18 +0800

    files uploads

---
+++ RareAnimals/Assets/Scripts/AudioNavManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioNavManager : BaseManager
+{
+    private AudioSource audioSource;
+
+    public override void InitView()
+    {
+        base.InitView();
+        audioSource = transform.Find("AudioSource").GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void AddAudioSource(AudioClip clip)
+    {
+
+    }
+}
