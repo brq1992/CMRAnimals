c14c38ef66e6143b3f0132d291b73fc199c5eefd
Home Pc <377818742@qq.com>
2018-02-04 00:51:34 +0800

    111111111111111

---
+++ RareAnimals/AndroidPluginProject/unityandroidplugin/src/main/java/com/example/unityandroidplugin/PluginClass.java
@@ -0,0 +1,13 @@
+package com.example.unityandroidplugin;
+
+/**
+ * Created by win 10 on 2/3/2018.
+ */
+
+public class PluginClass {
+
+    public static String GetPluginString(int number)
+    {
+        return "Number is " + number;
+    }
+}
