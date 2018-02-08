03a4d95c20e8ce5b67f7286ad76091f80da694ac
Home Pc <377818742@qq.com>
2018-02-08 21:44:47 +0800

    1111111111

---
+++ RareAnimals/Assets/Scripts/Android.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+
+
+public class Android : MonoBehaviour
+{
+    public static Android instance = null;
+#if UNITY_ANDROID
+    public static AndroidJavaObject jo;
+    public static AndroidJavaClass jc;
+#endif
+    void Awake()
+    {
+#if UNITY_ANDROID
+        //获取安卓类，用于掉用安卓的接口
+        jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+#endif
+    }
+
+#if UNITY_ANDROID
+    public static string GetWifiMacAddress()
+    {
+        return jc.CallStatic<string>("");
+        //jo.Call("openPdf");
+    }
+#elif UNITY_IPHONE
+    [DllImport("__Internal")]
+    private static extern void getWifiMacAddress();
+    public static void GetWifiMacAddress()
+    {
+        return getWifiMacAddress();
+    }
+#else
+    public static void GetWifiMacAddress(string path)
+    {
+        return "";
+    }
+#endif
+}
+
