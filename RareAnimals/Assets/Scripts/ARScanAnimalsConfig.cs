6605d85f5e678a7d7cfe46322f25bee1a6b0ef6b
admin <xbbhexbb@hotmail.com>
2018-05-11 16:23:54 +0800

    xiugai ...

--- RareAnimals/Assets/Scripts/ARScanAnimalsConfig.cs
+++ RareAnimals/Assets/Scripts/ARScanAnimalsConfig.cs
@@ -10,19 +10,9 @@ public class ARScanAnimalsConfig : ScriptableObject
 [System.Serializable]
 public class AnimalContents
 {
-    public List<AnimalContent> list = new List<AnimalContent>();
     public string Key = "";
     public GameObject prefab;
     public Sprite AnimalName;
     public string[] ANames;
-    public RawImage RawImage;
     public List<Texture2D> Texture2Ds = new List<Texture2D>();
-}
-
-[System.Serializable]
-public class AnimalContent
-{
-    public Sprite Bg;
-    public Sprite body;
-    public Sprite font;
 }
